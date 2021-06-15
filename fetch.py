85ceb4e4cbe298b16fa5fc466b341d8f82bbac5d
Ryan Petrello <ryan@ryanpetrello.com>
2021-06-15 09:00:18 -0400

    automate w/ gha

--- fetch.py
+++ fetch.py
@@ -11,21 +11,17 @@ OUT = 'deals.txt'
 URL = 'https://disneyworld.disney.go.com/special-offers/'
 
 def latest():
-    resp = requests.get(URL)
+    resp = requests.get(URL, headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0'
+    })
     soup = BeautifulSoup(resp.content, 'html.parser')
     with open(OUT, 'w') as f:
         for deal in soup.find_all('article'):
             f.write(deal.find_next('a')['href'])
             f.write('\n')
             details = deal.find_next(attrs={'class': 'details'}).text
-            f.write(details.encode('utf-8', 'ignore'))
+            f.write(details)
             f.write(('-' * 80) + '\n')
-
-def push():
-    subprocess.call(['git', 'add', OUT])
-    subprocess.call(['git', 'commit', '-m', datetime.utcnow().strftime('%Y-%m-%d')])
-    subprocess.call(['git', 'push', 'origin', 'master'])
 
 if __name__ == '__main__':
     latest()
-    push()
